51ab89ece32be24eb88b207d8f280f3f42bb21cc
AjnabhKoushik <ajnabhkoushikbaruah@gmail.com>
2026-07-01 12:34:54 +0530

    Add User class with name and portfolio attributes

---
+++ User.java
@@ -0,0 +1,25 @@
+public class User {
+
+    private String name;
+    private Portfolio portfolio;
+
+    public User(String name) {
+
+        this.name = name;
+        portfolio = new Portfolio();
+
+    }
+
+    public Portfolio getPortfolio() {
+
+        return portfolio;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+}
